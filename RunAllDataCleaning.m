7305be34acb78c4addc0e4c41692b1853fa0fa50
DapperVibes <31927305+DapperVibes@users.noreply.github.com>
2017-09-21 18:25:40 +0100

    Code calculating flux for one channel, not saving it yet, pump still swapped in Raw Data.

--- RunAllDataCleaning.m
+++ RunAllDataCleaning.m
@@ -9,6 +9,22 @@ resultsDir = '\\filestore.soton.ac.uk\users\cnd1g15\mydocuments\Projects\BioWaMe
 
 % list = DataCleaning(dataDir,resultsDir); % ,{'20170821112611.tdms'}
 
-data = CalculateFlux(resultsDir,'20170821112611',10);
+[data,fluxData,fluxTime] = CalculateFlux(resultsDir,'20170821112611',100);
+
+% timeData = data.data(:,1);
+% dt = data.data(2,1)-data.data(1,1);
+% pumpData = data.data(:,7);
+% massData = data.data(:,3);
+% massNorm = (massData-min(massData))/(max(massData)-min(massData));
+% 
+% [massData2] = EliminatePumpOnValues(pumpData,massData,dt);
+% massNorm2 = (massData2-min(massData2))/(max(massData2)-min(massData2));
+% 
+% figure(10)
+% plot(timeData,massData,timeData,massData2);
+
+figure(10)
+plot(fluxTime,fluxData)
+axis([0 100000 0 0.22])
 
 % dataFile = TDMS_getStruct([DataDir '\' list(1).name]);
